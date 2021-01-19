8f421b31bdb20f35a80b1d5f58d1ec69230b879a
Buse <trmn.buse@gmail.com>
2021-01-19 23:37:12 +0300

    add task description

--- Date3/Program.cs
+++ Date3/Program.cs
@@ -2,6 +2,9 @@
 
 namespace Date3
 {
+    //Bir bankada müşteri takibi yapmak istiyorsunuz.
+    //Musteri isimli bir Class oluşturunuz.Müşteriye istediğiniz özellikleri ekleyiniz. (Id, Ad, Soyad....)
+    //MusteriManager sınıfı oluşturunuz.Musteri parametresi alarak Musteri ekleme, listeleme,silme gibi metotları simule ediniz
     class Program
     {
         static void Main(string[] args)
